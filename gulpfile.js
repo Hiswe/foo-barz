7f0bb551bdbbdd19e7e8ce7500107ff378c17eb3
Hiswe <hiswehalya@gmail.com>
2018-09-10 11:33:20 +0700

    ICONS – fix clipPath issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const path = require('path')
 const gulp = require('gulp')
 const $ = require('gulp-load-plugins')()
 
@@ -9,49 +10,81 @@ const $ = require('gulp-load-plugins')()
 
 const materialName = /(?:outline|baseline)-([^\d]*)-[\d]+px/
 const icons = () => {
-  return (
-    gulp
-      .src(`./src/assets-source/icons/*.svg`)
-      .pipe(
-        $.cheerio({
-          run: $ => {
-            $(`#Bounding_Boxes`).remove()
-            $(`path[fill=none]`).remove()
-            $(`rect[fill=none]`).remove()
-            $(`rect[style="fill:none;"]`).remove()
+  return gulp
+    .src(`./src/assets-source/icons/*.svg`)
+    .pipe(
+      $.svgmin(file => {
+        const { relative } = file
+        const prefix = path.basename(relative, path.extname(relative))
+        return {
+          js2svg: {
+            pretty: true,
           },
-          parserOptions: {
-            xmlMode: true,
-          },
-        }),
-      )
-      // .pipe(
-      //   $.svgo({
-      //     plugins: [{ removeViewBox: false }],
-      //   }),
-      // )
-      .pipe(
-        $.rename(path => {
-          const { basename } = path
-          const isMaterialIcon = materialName.test(basename)
-          if (!isMaterialIcon) return
-          path.basename = materialName.exec(basename)[1].replace(/_/g, `-`)
-        }),
-      )
-      .pipe(
-        $.svgSymbols({
-          svgAttrs: {
-            class: `icon`,
-            xmlns: false,
-          },
-          class: `.icon--%f`,
-          templates: [`default-demo`, `default-vue`],
-        }),
-      )
-      .pipe($.if(/[.]vue$/, $.rename(`SvgIcons.vue`)))
-      .pipe($.if(/[.]vue$/, gulp.dest(`src/components/ui`)))
-      .pipe($.if(/[.]html$/, gulp.dest(`.`)))
-  )
+          plugins: [
+            { removeViewBox: false },
+            { removeDoctype: true },
+            { removeXMLProcInst: true },
+            { removeTitle: true },
+            { removeDesc: { removeAny: true } },
+            { convertTransform: {} },
+            // this prevent duplicated IDs when bundled in the same file
+            { cleanupIDs: { prefix: `${prefix}-` } },
+          ],
+        }
+      }),
+    )
+    .pipe(
+      $.cheerio({
+        run: $ => {
+          $(`#Bounding_Boxes`).remove()
+          $(`path[fill=none]`).remove()
+          $(`rect[fill=none]`).remove()
+          $(`rect[style="fill:none;"]`).remove()
+
+          const $clipPath = $(`clipPath`)
+          let $defs = $(`defs`)
+          const hasClipPath = $clipPath.length > 0
+          const hasDefs = $defs.length > 0
+          if (!hasClipPath) return
+          if (!hasDefs) {
+            $defs = $(`<defs></defs>`)
+            $defs.prependTo(`svg`)
+          }
+          function copyToDefs(i, el) {
+            const $el = $(el)
+            const $clone = $el.clone()
+            $clone.appendTo($defs)
+            $el.remove()
+          }
+          if (hasClipPath) $clipPath.each(copyToDefs)
+        },
+        parserOptions: {
+          xmlMode: true,
+        },
+      }),
+    )
+
+    .pipe(
+      $.rename(path => {
+        const { basename } = path
+        const isMaterialIcon = materialName.test(basename)
+        if (!isMaterialIcon) return
+        path.basename = materialName.exec(basename)[1].replace(/_/g, `-`)
+      }),
+    )
+    .pipe(
+      $.svgSymbols({
+        svgAttrs: {
+          class: `icon`,
+          xmlns: false,
+        },
+        class: `.icon--%f`,
+        templates: [`default-demo`, `default-vue`],
+      }),
+    )
+    .pipe($.if(/[.]vue$/, $.rename(`SvgIcons.vue`)))
+    .pipe($.if(/[.]vue$/, gulp.dest(`src/components/ui`)))
+    .pipe($.if(/[.]html$/, gulp.dest(`.`)))
 }
 icons.description = `build SVG icons`
 exports[`build:icons`] = icons
